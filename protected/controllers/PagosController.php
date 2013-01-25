9a79b71fe879d6d28705acee5eb055905db8dadc
unknown <Usuario@.(none)>
2013-01-24 21:33:51 -0400

    a

---
+++ protected/controllers/PagosController.php
@@ -0,0 +1,89 @@
+<?php
+
+class PagosController extends GxController {
+
+
+	public function actionView($id) {
+		$this->render('view', array(
+			'model' => $this->loadModel($id, 'Pagos'),
+		));
+	}
+	
+	//Agregado para ver desde la ventana MODAL
+	public function actionVer($id) {
+		//Para mostrar en la ventana modal solo el content
+		$this->layout = '//layouts/iframe';
+		
+		$this->render('view_admin', array(
+			'model' => $this->loadModel($id, 'Pagos'),
+		));
+	}
+	
+
+	public function actionCreate() {
+		$model = new Pagos;
+
+
+		if (isset($_POST['Pagos'])) {
+			$model->setAttributes($_POST['Pagos']);
+
+			if ($model->save()) {
+				if (Yii::app()->getRequest()->getIsAjaxRequest())
+					Yii::app()->end();
+				else
+					//Cierra la venta Modal
+					echo CHtml::script("parent.cerrarModal();");
+			}
+		}
+		//Para mostrar en la ventana modal solo el content
+		$this->layout = '//layouts/iframe';
+		$this->render('create', array( 'model' => $model));
+	}
+
+	public function actionUpdate($id) {
+		$model = $this->loadModel($id, 'Pagos');
+
+
+		if (isset($_POST['Pagos'])) {
+			$model->setAttributes($_POST['Pagos']);
+
+			if ($model->save()) {
+				//Cierra la venta Modal
+				//echo CHtml::script("parent.cerrarModal();");
+				echo CHtml::script("parent.location.reload();");
+				//$this->redirect(array('view', 'id' => $model->id));
+			}
+		}
+		
+		//Para mostrar en la ventana modal solo el content
+		$this->layout = '//layouts/iframe';
+		
+		$this->render('update', array(
+				'model' => $model,
+				));
+	}
+
+	public function actionDelete($id) {
+		if (Yii::app()->getRequest()->getIsPostRequest()) {
+			$this->loadModel($id, 'Pagos')->delete();
+
+			if (!Yii::app()->getRequest()->getIsAjaxRequest())
+				$this->redirect(array('admin'));
+		} else
+			throw new CHttpException(400, Yii::t('app', 'Requerimiento inválido.'));
+	}
+	
+	
+	public function actionIndex() {
+		$model = new Pagos('search');
+		$model->unsetAttributes();
+
+		if (isset($_GET['Pagos']))
+			$model->setAttributes($_GET['Pagos']);
+
+		$this->render('index', array(
+			'model' => $model,
+		));
+	}
+
+}
---
+++ protected/views/pagos/index.php
@@ -0,0 +1,99 @@
+<?php
+
+$this->breadcrumbs = array(
+	$model->label(2) => array('index'),
+	Yii::t('app', 'Manage'),
+);
+
+$this->menu = array(
+		array('label'=>Yii::t('app', 'Create') . ' ' . $model->label(), 'url'=>array('create')),
+	);
+
+Yii::app()->clientScript->registerScript('search', "
+
+$('.search-button').click(function(){
+	$('.search-form').toggle();
+	return false;
+});
+$('.search-form form').submit(function(){
+	$.fn.yiiGridView.update('pagos-grid', {
+		data: $(this).serialize()
+	});
+	return false;
+});
+");
+?>
+<div class="form">
+<h3><?php echo Yii::t('app', 'Manage') . ' ' . GxHtml::encode($model->label(2)); ?></h3>
+
+<p>
+	Si lo desea, puede introducir un operador de comparación (&lt;, &lt;=, &gt;, &gt;=, &lt;&gt; or =) al comienzo de cada uno de los valores de su búsqueda para especificar cómo la comparación se debe hacer.
+</p>
+
+<?php echo GxHtml::link(Yii::t('app', 'Búsqueda Avanzada'), '#', array('class' => 'search-button')); ?>
+<div class="search-form">
+<?php $this->renderPartial('_search', array(
+	'model' => $model,
+)); ?>
+</div><!-- search-form -->
+
+<?php $this->widget('zii.widgets.grid.CGridView', array(
+	'id' => 'pagos-grid',
+	'dataProvider' => $model->search(),
+	//'filter' => $model,
+	'columns' => array(
+		'id',
+		array(
+				'name'=>'compromiso_detalle_id',
+				'value'=>'GxHtml::valueEx($data->compromisoDetalle)',
+				'filter'=>GxHtml::listDataEx(DetallesCompromisos::model()->findAllAttributes(null, true)),
+				),
+		array(
+				'name'=>'tipo_pago_id',
+				'value'=>'GxHtml::valueEx($data->tipoPago)',
+				'filter'=>GxHtml::listDataEx(TiposPagos::model()->findAllAttributes(null, true)),
+				),
+		'cheque_numero',
+		'cheque_rut',
+		'cheque_plaza',
+		/*
+		array(
+				'name'=>'cheque_banco_id',
+				'value'=>'GxHtml::valueEx($data->chequeBanco)',
+				'filter'=>GxHtml::listDataEx(Bancos::model()->findAllAttributes(null, true)),
+				),
+		'observaciones',
+		array(
+				'name'=>'tasa_interes_id',
+				'value'=>'GxHtml::valueEx($data->tasaInteres)',
+				'filter'=>GxHtml::listDataEx(TasasInteres::model()->findAllAttributes(null, true)),
+				),
+		'interes_cobrado',
+		'fecha_pago',
+		array(
+				'name'=>'responsable_id',
+				'value'=>'GxHtml::valueEx($data->responsable)',
+				'filter'=>GxHtml::listDataEx(User::model()->findAllAttributes(null, true)),
+				),
+		'cheque_fecha',
+		'valor_cuota',
+		'pago_total',
+		'cheque_serie',
+		'cheque_rut_serie',
+		'estado',
+		*/
+		array(
+            'class' => 'CButtonColumn',
+            'template'=>'{view}{update}{delete}',
+            'header' => 'Acción',
+            'buttons'=>array(                  
+
+                'view'=>
+                        array(    
+                            'url'=>'$this->grid->controller->createUrl("ver", array("id"=>$data->id))',                              
+                        ),          
+            ),   
+        ),
+	),
+)); ?>
+</div>
